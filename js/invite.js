85c29197a414b16352cd64424ed08b07c699f514
teiresias22 <teiresias1987@gmail.com>
2024-10-14 18:45:20 +0900

    create invite page

--- js/invite.js
+++ js/invite.js
@@ -1,9 +1,16 @@
-window.onload = function() {
+document.getElementById('openLink').addEventListener('click', function() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const inviteCode = urlParams.get('9c0f5f62-6b42-49dc-8e36-50765f323661');
 
-    if (inviteCode) {
-        document.body.innerHTML += `<p>Your invite code is: ${inviteCode}</p>`;
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    var linkToOpen = urlParams;
+
+    if (/android/i.test(userAgent)) {
+        window.open('googlechrome://' + linkToOpen, '_blank');
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        window.open('https://teiresias22.github.io/invite', '_blank'); // iOS Safari will handle this
+    } else {
+        window.open(linkToOpen, '_blank');
     }
-};
+});
